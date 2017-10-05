f733f33b0b113a4b39f7d0942e92a7eff5ab6cb9
Josep <josep.a.95@gmail.com>
2017-10-05 12:18:12 +0200

    Created path method

--- search/node.py
+++ search/node.py
@@ -8,7 +8,14 @@ class Node:
     def __str__(self):
         return '--Node {0}--\n\tParent: {1}\n\tAction: {2}\n\tCost: {3}' \
                .format(self.state, self.parent, self.action, self.cost)
+    
+    def path(self):
+        if self.parent is None:
+            return []
+        return self.parent.path() + [self.action]
 
-if __name__ == "__main__":
-    n = Node("example state")
-    print n
+if __name__ == '__main__':
+    n1 = Node('n1', None, 'South')
+    n2 = Node('n2', n1, 'West')
+    print n2.path()
+    
