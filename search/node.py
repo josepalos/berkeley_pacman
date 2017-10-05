c98d1b79955cdab527cec27feb388cd153a0ce17
Josep <josep.a.95@gmail.com>
2017-10-05 09:48:20 +0200

    Added class node

---
+++ search/node.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, state, parent=None, action=None, cost=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.cost = cost
+
+    def __str__(self):
+        return '--Node {0}--\n\tParent: {1}\n\tAction: {2}\n\tCost: {3}' \
+               .format(self.state, self.parent, self.action, self.cost)
+
+if __name__ == "__main__":
+    n = Node("example state")
+    print n
